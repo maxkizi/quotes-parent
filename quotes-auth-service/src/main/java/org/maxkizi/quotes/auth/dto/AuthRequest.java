e82cfde568994f9ec6c904947eb773797143618b
MKizilov <vtb4055428>
2023-12-11 17:17:35 +0300

    docker job

--- quotes-auth-service/src/main/java/org/maxkizi/quotes/auth/dto/AuthRequest.java
+++ quotes-auth-service/src/main/java/org/maxkizi/quotes/auth/dto/AuthRequest.java
@@ -2,9 +2,13 @@ package org.maxkizi.quotes.auth.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class AuthRequest {
     private String login;
     private String password;
--- quotes-auth-service/src/main/java/org/maxkizi/quotes/auth/dto/SignupRequest.java
+++ quotes-auth-service/src/main/java/org/maxkizi/quotes/auth/dto/SignupRequest.java
@@ -2,12 +2,15 @@ package org.maxkizi.quotes.auth.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class SignupRequest {
     private String login;
     private String password;
     private String name;
-
 }
